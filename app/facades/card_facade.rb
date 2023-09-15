ad324fd6c95d3ef7f7148d0da05b02823641bc46
Derek Chavez <frrenose17@yahoo.com>
2023-09-14 18:29:34 -0600

    feat: working on image url

--- app/facades/card_facade.rb
+++ app/facades/card_facade.rb
@@ -1,13 +1,13 @@
 class CardFacade
   def self.random_card
     data = SpellbinderService.get_random_card
-    attributes = data[:data][:attributes]
+    attributes = data[:data][0][:attributes]
     Card.new(attributes)
   end
 
   def self.get_card(id)
     data = SpellbinderService.get_card(id)
-    attributes = data[:data][:attributes]
+    attributes = data[:data][0][:attributes]
     Card.new(attributes)
   end
 end
