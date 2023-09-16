c2bd182af9b6018166bce8a664007998c0cffc4e
Matthew Lim <matlim200@gmail.com>
2023-09-15 18:25:21 -0700

    add collection show formatting

--- spec/features/collections/show_spec.rb
+++ spec/features/collections/show_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe "Collections Show Page" do
       single_collection = File.read('spec/fixtures/get_single_collection.json')
       stub_request(:get, "http://localhost:3000/api/v0/collections/ARN").to_return(status: 200, body: single_collection)
       visit "/collections/ARN"
+
     end
 
-    it "displays the name and attributes" do
-      expect(page).to have_content("Abu Ja'far")
+    it "displays the card image" do
+      expect(page).to have_selector("img[src='http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=968&type=card']")
+      # expect(page).to have_content("Abu Ja'far")
       # expect(page).to have_content("Number of Cards: 3")
       # expect(page).to have_content("Set Logo: https://img.scryfall.com/sets/3dn.svg?1555905600")
     end
---
+++ spec/features/dashboards/dashboard_index_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Dashboard Index Page" do
+  describe "#dashboard index" do
+    xit "displays a link for each deck image" do
+      visit collections_index_path
+
+    end
+  end
+end
