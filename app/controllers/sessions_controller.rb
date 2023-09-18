f5dfe9532a4ceedb77a29a8a7df1ec7821c13fdf
Derek Chavez <frrenose17@yahoo.com>
2023-09-18 16:18:14 -0600

    feat; add dashboard view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def login
-    user = UserFacade.get_user(params[:email], params[:password])
+    user = UserFacade.find_user(params[:username], params[:password])
     if user.nil?
       flash[:error] = "Invalid credentials"
       redirect_to "/login"
     else
       session[:user_id] = user.id
-      redirect_to "/dashboard/#{user.id}"
+      redirect_to "/dashboard"
     end
   end
 end
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -3,18 +3,30 @@ require "rails_helper"
 RSpec.describe "Login Form Page" do
   it "allows a user to login" do
     user = File.read('spec/fixtures/get_user.json')
-    stub_request(:get, "http://localhost:3000/api/v0/login").to_return(status: 200, body: user)
+    # stub_request(:get, "http://localhost:3000/api/v0/login//password").to_return(status: 200, body: user)
+
+    stub_request(:post, "http://localhost:3000/api/v0/login/turing_test,password").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Content-Length'=>'0',
+      'User-Agent'=>'Faraday v2.7.11'
+      }).
+    to_return(status: 200, body: user, headers: {})
+
+    stub_request(:get, "http://localhost:3000/api/v0/login//password")
 
     visit '/login'
 
     expect(page).to have_content("Login")
-    expect(page).to have_content("Email")
+    expect(page).to have_content("User Name")
     expect(page).to have_content("Password")
 
-    fill_in :email, with: "turing_test@turing.edu"
+    fill_in :username, with: "turing_test"
     fill_in :password, with: "password"
     click_on "Login"
 
-    expect(current_path).to eq("/dashboard/1")  
+    expect(current_path).to eq("/dashboard")  
   end
 end
